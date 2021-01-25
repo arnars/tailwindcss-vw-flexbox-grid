bae71aa44018a2821732c95bf5475de798d2fbf4
Anders Hallundbæk <anders.hallundbaek@gmail.com>
2021-01-25 21:29:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# Flexbox grid system based on View width for Tailwind CSS
+
+## Requirements
+
+This plugin requires Tailwind CSS 2.0 or later.
+
+## Installation
+
+```bash
+yarn add tailwindcss-vw-flexbox-grid
+```
+
+## Usage
+
+This plugin uses the `screen` key in your Tailwind config’s `theme`. You can choose to configure it for only some of the screens, if your grid is consistent throughout multiple screens.
+
+Make sure the keys of the screens in your config are matching the options provided to the plugin.
+
+```js
+// tailwind.config.js
+module.exports = {
+    theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
+    },
+    plugins: [require('tailwindcss-vw-flexbox-grid')()],
+};
+```
+
+### Quick example
+
+```html
+<!-- Basic container -->
+<div class="w-full mx-auto space-y-12 min-w-page max-w-page">
+    <!-- Row (only room for 4 columns below :md) -->
+    <div class="row md:hidden">
+        <div class="col col-2">Column</div>
+        <div class="col col-1 indent-left-1">Column with left indent</div>
+    </div>
+
+    <!-- Row (only room for 6 columns below :lg) -->
+    <div class="row hidden md:flex">
+        <div class="col col-2">Column</div>
+        <div class="col col-2 indent-right-1">Column with right indent</div>
+    </div>
+
+    <!-- Row (room for 12 columns) -->
+    <div class="row hidden lg:flex">
+        <div class="col col-6">Column</div>
+        <div class="col-gapless col-gapless-2">Column without gaps</div>
+        <div class="col col-4">Column</div>
+    </div>
+</div>
+```
+
+### Default options
+
+In the default options 4 screens are configured. All screen-related variables are in Arrays, where the index is corresponding the screen index.
+
+```js
+const defaultOptions = {
+    // Choose which screens to change settings for
+    screens: [null, 'md', 'lg', '2xl'],
+    // Number of column for different screens
+    columns: [4, 6, 12, 12],
+    // Page widths for different screens (if anything else but pixels, set as string. Pixels will be converted to em)
+    pageWidths: ['100vw', '100vw', '100vw', 1536],
+    // Column gaps for different screens
+    columnGaps: [12, 12, 12, 12],
+    // Page gaps for different screens
+    pageGaps: [12, 12, 12, 12],
+    // Page max width
+    pageMaxWidth: 1536,
+    // Page min width
+    pageMinWidth: 320,
+};
+```
+
+### Column classes
+
+```css
+.row {
+    /* Handy wrapper for columns */
+}
+.col {
+    /* Configuration class - needs to be there if you use col-x */
+}
+.col-gapless {
+    /* Configuration class - needs to be there if you use col-gapless-x */
+}
+.col-x {
+    /* Column - The x decides the width of your column */
+}
+.col-gapless-x {
+    /* Column - The x decides the width of your column */
+}
+.indent-left-x {
+    /* Indentation for columns - The x decides how many columns you want to indent */
+}
+.indent-right-x {
+    /* Indentation for columns - The x decides how many columns you want to indent */
+}
+```
+
+### Additional classes - tooling
+
+```css
+.gap-padding {
+    /* Sets the current gap width as padding */
+}
+.gap-padding-x {
+    /* Sets the current gap width as padding - left / right */
+}
+.gap-padding-y {
+    /* Sets the current gap width as padding - top / bottom */
+}
+.gap-margin {
+    /* Sets the current gap width as margin */
+}
+.gap-margin-x {
+    /* Sets the current gap width as margin - left / right */
+}
+.gap-margin-y {
+    /* Sets the current gap width as margin - top / bottom */
+}
+```
+
+## Gotchas
+
+When using `.col-x`, `box-sizing` is set to `content-box` and padding is used to archieve the gap-effect.
+This is because I would like to be able to use the `.indent-left/right-x` which works with margin.
+
+When using `.col-gapless-x`, no gap is needed and `box-sizing` is set to `border-box`.
+
+It often makes sense to add a child `div` to the column-div in order to avoid overriding essential behavior.
---
+++ index.js
@@ -0,0 +1,335 @@
+const plugin = require('tailwindcss/plugin');
+
+const remInPx = (num, name = false) => ({
+    [name ? name : num]: `${num / 16}rem`,
+});
+
+const defaultOptions = {
+    screens: [null, 'md', 'lg', '2xl'],
+    columns: [4, 6, 12, 12],
+    pageWidths: ['100vw', '100vw', '100vw', 1440],
+    columnGaps: [12, 12, 12, 12],
+    pageGaps: [12, 12, 12, 12],
+    pageMaxWidth: 1440,
+    pageMinWidth: 320,
+};
+
+module.exports = plugin.withOptions(
+    function (userOptions = {}) {
+        return function ({ theme, addUtilities }) {
+            // Merge options
+            const options = { ...defaultOptions, ...userOptions };
+
+            // Generate media queries to place utilities in
+            const mediaQueries = options.screens.reduce((acc, item) => {
+                const mediaQuery = item
+                    ? `@media (min-width: ${theme(`screens.${item}`)})`
+                    : ``;
+                acc = [...acc, mediaQuery];
+                return acc;
+            }, []);
+
+            // .col and row
+            addUtilities(
+                {
+                    '.col': {
+                        boxSizing: 'content-box',
+                        flexGrow: 0,
+                        flexShrink: 0,
+                        ...mediaQueries.reduce(
+                            (acc, item, index) => ({
+                                ...acc,
+                                [item]: {
+                                    paddingLeft: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                    paddingRight: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                },
+                            }),
+                            {}
+                        ),
+                    },
+                    '.col-gapless': {
+                        flexGrow: 0,
+                        flexShrink: 0,
+                    },
+                    '.row': {
+                        display: 'flex',
+                        ...mediaQueries.reduce(
+                            (acc, item, index) => ({
+                                ...acc,
+                                [item]: {
+                                    marginLeft: `${
+                                        options.pageGaps[index] / 16
+                                    }rem`,
+                                    marginRight: `${
+                                        options.pageGaps[index] / 16
+                                    }rem`,
+                                },
+                            }),
+                            {}
+                        ),
+                    },
+                },
+                { variants: ['responsive'] }
+            );
+
+            // .gap-padding / .gap-margin
+            addUtilities(
+                {
+                    '.gap-padding': {
+                        ...mediaQueries.reduce(
+                            (acc, item, index) => ({
+                                ...acc,
+                                [item]: {
+                                    padding: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                },
+                            }),
+                            {}
+                        ),
+                    },
+                    '.gap-padding-x': {
+                        ...mediaQueries.reduce(
+                            (acc, item, index) => ({
+                                ...acc,
+                                [item]: {
+                                    paddingLeft: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                    paddingRight: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                },
+                            }),
+                            {}
+                        ),
+                    },
+                    '.gap-padding-y': {
+                        ...mediaQueries.reduce(
+                            (acc, item, index) => ({
+                                ...acc,
+                                [item]: {
+                                    paddingTop: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                    paddingBottom: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                },
+                            }),
+                            {}
+                        ),
+                    },
+                    '.gap-margin': {
+                        ...mediaQueries.reduce(
+                            (acc, item, index) => ({
+                                ...acc,
+                                [item]: {
+                                    margin: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                },
+                            }),
+                            {}
+                        ),
+                    },
+                    '.gap-margin-x': {
+                        ...mediaQueries.reduce(
+                            (acc, item, index) => ({
+                                ...acc,
+                                [item]: {
+                                    marginLeft: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                    marginRight: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                },
+                            }),
+                            {}
+                        ),
+                    },
+                    '.gap-margin-y': {
+                        ...mediaQueries.reduce(
+                            (acc, item, index) => ({
+                                ...acc,
+                                [item]: {
+                                    marginTop: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                    marginBottom: `${
+                                        options.columnGaps[index] / 16
+                                    }rem`,
+                                },
+                            }),
+                            {}
+                        ),
+                    },
+                },
+                { variants: ['responsive'] }
+            );
+
+            // .indent-left/right-x
+            addUtilities(
+                {
+                    ...mediaQueries.reduce((qAcc, qItem, qIndex) => {
+                        // Get columns length
+                        const columns = options.columns[qIndex];
+
+                        // Get page width
+                        const pageWidth =
+                            typeof options.pageWidths[qIndex] === 'string'
+                                ? options.pageWidths[qIndex]
+                                : `${options.pageWidths[qIndex] / 16}em`;
+
+                        // Get page gap
+                        const pageGap = `${options.pageGaps[qIndex] / 16}rem`;
+
+                        // Generate indent rules
+                        const indentRules = [...new Array(columns)].reduce(
+                            (cAcc, cItem, cIndex) => {
+                                // Only up to one below max
+                                if (cIndex + 1 < columns) {
+                                    // Number
+                                    const count = cIndex + 1;
+
+                                    // Accumulate
+                                    cAcc = {
+                                        ...cAcc,
+                                        [`.indent-left-${count}`]: {
+                                            marginLeft: `calc((${pageWidth} - 2 * ${pageGap}) / ${columns} * ${count})`,
+                                        },
+                                        [`.indent-right-${count}`]: {
+                                            marginRight: `calc((${pageWidth} - 2 * ${pageGap}) / ${columns} * ${count})`,
+                                        },
+                                    };
+                                }
+                                return cAcc;
+                            },
+                            {}
+                        );
+
+                        // Accumulate
+                        qAcc = {
+                            ...qAcc,
+                            [qItem]: indentRules,
+                        };
+
+                        return qAcc;
+                    }, {}),
+                },
+                { variants: ['responsive'] }
+            );
+
+            // .col-x / .col-gapless-x
+            addUtilities(
+                {
+                    ...mediaQueries.reduce((qAcc, qItem, qIndex) => {
+                        // Get columns length
+                        const columns = options.columns[qIndex];
+
+                        // Get page width
+                        const pageWidth =
+                            typeof options.pageWidths[qIndex] === 'string'
+                                ? options.pageWidths[qIndex]
+                                : `${options.pageWidths[qIndex] / 16}em`;
+
+                        // Get page min width
+                        const pageMinWidth = `${options.pageMinWidth / 16}em`;
+
+                        // Get page gap
+                        const pageGap = `${options.pageGaps[qIndex] / 16}rem`;
+
+                        // Get column gap
+                        const columnGap = `${
+                            options.columnGaps[qIndex] / 16
+                        }rem`;
+
+                        // Generate column rules
+                        const colRules = [...new Array(columns)].reduce(
+                            (cAcc, cItem, cIndex) => {
+                                if (cIndex < columns) {
+                                    // Number
+                                    const count = cIndex + 1;
+
+                                    // Accumulate
+                                    cAcc = {
+                                        ...cAcc,
+                                        [`.col-${count}`]: {
+                                            width: `calc(((${pageWidth} - 2 * ${pageGap}) / ${columns} * ${count}) - (2 * ${columnGap}))`,
+                                            minWidth: `calc(((${pageMinWidth} - 2 * ${pageGap}) / ${columns} * ${count}) - (2 * ${columnGap}))`,
+                                        },
+                                        [`.col-gapless-${count}`]: {
+                                            width: `calc(((${pageWidth} - 2 * ${pageGap}) / ${columns} * ${count}))`,
+                                            minWidth: `calc(((${pageMinWidth} - 2 * ${pageGap}) / ${columns} * ${count}))`,
+                                        },
+                                    };
+                                }
+                                return cAcc;
+                            },
+                            {}
+                        );
+
+                        // Accumulate
+                        qAcc = {
+                            ...qAcc,
+                            [qItem]: colRules,
+                        };
+
+                        return qAcc;
+                    }, {}),
+                },
+                { variants: ['responsive'] }
+            );
+        };
+    },
+    function (userOptions = {}) {
+        // Merge options
+        const options = { ...defaultOptions, ...userOptions };
+
+        // column-gap-xx
+        const columnGaps = options.screens.reduce(
+            (acc, item, index) => ({
+                ...acc,
+                ...remInPx(
+                    options.columnGaps[index],
+                    `column-gap${item ? `-${item}` : ''}`
+                ),
+            }),
+            {}
+        );
+
+        // page-gap-xx
+        const pageGaps = options.screens.reduce(
+            (acc, item, index) => ({
+                ...acc,
+                ...remInPx(
+                    options.pageGaps[index],
+                    `page-gap${item ? `-${item}` : ''}`
+                ),
+            }),
+            {}
+        );
+
+        return {
+            theme: {
+                extend: {
+                    spacing: {
+                        ...columnGaps,
+                        ...pageGaps,
+                    },
+                    maxWidth: {
+                        ...remInPx(options.pageMaxWidth, 'page'),
+                    },
+                    minWidth: {
+                        ...remInPx(options.pageMinWidth, 'page'),
+                    },
+                },
+            },
+        };
+    }
+);
